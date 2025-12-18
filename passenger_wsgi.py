db59bdfc6966a9dfe6078877d688c77f2c04e9b3
Mariogit20 <halleymariomen@gmail.com>
2025-12-18 12:57:37 +0300

    mv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the sys.path
+# Replace '/path/to/your/project' with your actual path
+sys.path.insert(0, os.path.dirname(__file__))
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
